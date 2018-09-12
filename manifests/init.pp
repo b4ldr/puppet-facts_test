1169f6fcbcc9569a13a0afb878ce3c2faaf3a353
John Bond <github@johnbond.org>
2018-09-12 17:14:11 +0100

    initial push

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+class facts_test (
+  $hiera_ipaddress,
+  $facts_ipaddress = $::ipaddress,
+) {
+  file {'/tmp/hiera_ipaddress':
+    ensure  => file,
+    content => $hiera_ipaddress,
+  }
+  file {'/tmp/facts_ipaddress':
+    ensure  => file,
+    content => $facts_ipaddress,
+  }
+}
---
+++ spec/classes/facts_test_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'facts_test' do
+  let(:node) { 'foobar.example.com' }
+  let(:params) { {} }
+
+  # Puppet::Util::Log.level = :debug
+  # Puppet::Util::Log.newdestination(:console)
+  # This will need to get moved
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+      it { pp catalogue.resources }
+
+      describe 'check default config' do
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file('/tmp/hiera_ipaddress').with(
+            ensure: 'file',
+            content: '172.16.254.254',
+          )
+        end
+        it do
+          is_expected.to contain_file('/tmp/facts_ipaddress').with(
+            ensure: 'file',
+            content: '172.16.254.254',
+          )
+        end
+      end
+    end
+  end
+end
